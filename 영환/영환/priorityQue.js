9cd6dc104138cc173e1a0690fae403ca4775e379
yhsul <yhsul@nshc.net>
2020-06-20 01:26:15 +0900

    [영환] 추가 문제풀이
    - 1000 -> 백준 표준
    - 9498 -> 백준 표준2
    - 이중우선순위 큐
    - 숫자야구
    - 우선순위 큐 구현
    
    <description>
    
    <issue-number>

--- 영환/영환/priorityQue.js
+++ 영환/영환/priorityQue.js
@@ -1,12 +1,15 @@
-const priorityQue = (arr) =>{
+const priorityQue = (arr,conditionFn) =>{
     let heap = [""].concat(arr);
     let que = [];
+    if(typeof conditionFn !== "function" ){
+        conditionFn = function(a,b){
+            return a>b;
+        }
+    }
     while(heap.length!==1){
-        for(let i = heap.length -1 ; i >0 ; i--){
-            if(heap[i]>heap[Math.floor(i/2)]){
-                let tmp = heap[i];
-                heap[Math.floor(i/2)] = heap[i];
-                heap[i] = tmp;
+        for(let i = 2 ; i <heap.length ; i++){
+            if(conditionFn(heap[i],heap[Math.floor(i/2)])){
+                queChange(heap,i,conditionFn);
             }
         }
         que.push(heap[1]);
@@ -15,3 +18,15 @@ const priorityQue = (arr) =>{
     }
     return que;
 }
+const queChange = (arr,idx,conditionFn)=>{
+    const newIdx = Math.floor(idx/2);
+    let tmp = arr[idx];
+    arr[idx] = arr[newIdx];
+    arr[Math.floor(idx/2)] = tmp;
+    if(newIdx !==1 && conditionFn(arr[newIdx],arr[Math.floor(newIdx/2)])){
+        queChange(arr,newIdx,conditionFn);
+    }
+}
+
+// 사용법 배열을 1번째 변수에 넣고 두번째는 두개를 비교하여 참이되는 조건을 넣으면 된다. 
+// 함수 default값은 a>b === 큰거 우선
